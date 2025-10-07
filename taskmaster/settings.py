be5cb6b2062f8b436132f0234b8de89b8946bd74
Val916 <junsofdev@proton.me>
2025-10-07 11:29:23 +0100

    Set DEBUG to False for production readiness

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ci-taskmaster-final-dff252a883f7.herokuapp.com", "127.0.0.1", "localhost"]
 CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com"]
